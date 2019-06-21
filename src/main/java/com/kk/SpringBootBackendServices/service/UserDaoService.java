8018754951de836382ac3d1d82b855f361a15780
K. Kartikeya <kkartikeya1998@gmail.com>
2019-06-21 22:49:06 +0530

    Creates UserServices

---
+++ src/main/java/com/kk/SpringBootBackendServices/service/UserDaoService.java
@@ -0,0 +1,38 @@
+package com.kk.SpringBootBackendServices.service;
+
+
+import com.kk.SpringBootBackendServices.model.User;
+import com.kk.SpringBootBackendServices.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserDaoService {
+    @Autowired
+    UserRepository userRepository;
+
+    public List<User> findAllUsers(){
+        return userRepository.findAll();
+    }
+
+    public Optional<User> findUserById(Long id){
+        return userRepository.findById(id);
+    }
+
+    public User createUser(User user){
+        userRepository.save(user);
+        return user;
+    }
+
+    public User updateUser(User user){
+        userRepository.save(user);
+    }
+
+    public void deleteUser(User user){
+        userRepository.delete(user);
+    }
+
+}
