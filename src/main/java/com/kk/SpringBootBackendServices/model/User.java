7904b518e0f808ae51c20b42fb299958174a27b5
K. Kartikeya <kkartikeya1998@gmail.com>
2019-06-21 21:43:31 +0530

    Creates User Entity

---
+++ src/main/java/com/kk/SpringBootBackendServices/model/User.java
@@ -0,0 +1,55 @@
+package com.kk.SpringBootBackendServices.model;
+
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private  Long id;
+    private String firstName;
+    private String lastName;
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    public User(){
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString(){
+        return ("User :{" +
+                "'id=" + this.id + "',"+
+                "'firstName=" + this.firstName+"',"+
+                "'lastName=" + this.lastName+"'");
+    }
+}
