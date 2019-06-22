0c379eb324f9b6a67c473c41b977da0ddf75fb15
K. Kartikeya <kkartikeya1998@gmail.com>
2019-06-22 12:26:20 +0530

    Will overrite this

--- src/main/java/com/kk/SpringBootBackendServices/controller/UserController.java
+++ src/main/java/com/kk/SpringBootBackendServices/controller/UserController.java
@@ -1,42 +1,4 @@
 package com.kk.SpringBootBackendServices.controller;
 
-import com.kk.SpringBootBackendServices.model.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import com.kk.SpringBootBackendServices.services.UserDaoService;
-
-import java.util.List;
-
-@RestController
 public class UserController {
-
-    @Autowired
-    UserDaoService userDaoService;
-
-    @GetMapping("/users")
-    public List<User> getAllUsers(){
-        return userDaoService.findAllUsers();
-    }
-
-    @GetMapping("/user/{id}")
-    public User getUserById(@PathVariable("id") Long id){
-        return userDaoService.findUserById(id);
-    }
-
-    @PostMapping("/user")
-    public User createUser(@RequestBody User user){
-        return userDaoService.createUser(user);
-    }
-
-    @PutMapping("/user")
-    public User updateUserById(@RequestBody User user){
-        return userDaoService.updateUser(user);
-    }
-
-    @DeleteMapping("/user/{id}")
-    public void deleteUserById(@PathVariable("id") Long id){
-        userDaoService.deleteUser(userDaoService.findUserById(id));
-    }
-
 }
